909a3f3c3b3d4fd76932c9f618742ca1257cef79
Oviya Jaisankar <120849400+oviyaj@users.noreply.github.com>
2023-02-24 19:08:34 -0500

    Set the deadband to 1

--- src/main/java/frc/robot/subsystems/JoystickSubsystem.java
+++ src/main/java/frc/robot/subsystems/JoystickSubsystem.java
@@ -17,10 +17,15 @@ public class JoystickSubsystem extends SubsystemBase {
     private double operatorRightX;
     private double operatorRightY;
 
+
     /** Creates a new JoystickSubsystem. */
     public JoystickSubsystem(CommandXboxController driverController, CommandXboxController operatorController) {
         this.driverController = driverController;
         this.operatorController = operatorController;
+        setDeadband(1);
+    }
+
+    private void setDeadband(double d) {
     }
 
     @Override
